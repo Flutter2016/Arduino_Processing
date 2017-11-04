482785e81653abcd70b1e5e7b2a2ec47777392bf
Flutter2016 <3158445@student.ocadu.ca>
2017-11-04 16:41:12 -0400

    Update working_arduino_code.ino

--- working_arduino_code.ino
+++ working_arduino_code.ino
@@ -1,3 +1,9 @@
+// Arduino Code 
+// Im making a toggle controller that should move an ellipse in Processing. 
+// The toggle is connected to A0 and A1 that give me the X and Y values that I 
+// then send to Processing using Serial.Write
+
+
 void setup() {
   Serial.begin(9600);
   pinMode(2, INPUT); 
