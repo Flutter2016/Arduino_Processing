3e9cd936848c81dcdb41c5f1b7e968400d19d4f4
Flutter2016 <3158445@student.ocadu.ca>
2017-11-04 16:37:38 -0400

    Add files via upload

---
+++ working_arduino_code.ino
@@ -0,0 +1,18 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, INPUT); 
+}
+
+void loop(){
+
+  int xValue = analogRead(A0)/4;        
+  int yValue = analogRead(A1)/4;  
+  
+  
+  Serial.write(xValue);
+  Serial.write(yValue);
+
+
+  delay(100);
+}
+
